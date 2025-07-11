21ae55846e579e8881011e51276ce700ec1950fe
jpwiggins <your-email@example.com>
2025-07-11 14:48:47 -0500

    Update dependencies and project files

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,26 +1,23 @@
 import { defineConfig } from 'vite'; 
-import react from '@vitejs/plugin-react-swc'; 
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-export default defineConfig({ 
-  plugins: [ 
-    react(), 
-    viteStaticCopy({ 
-      targets: [ 
-        { 
-          src: 'public/_redirects', 
-          dest: '.' 
-        } 
-      ] 
-    }) 
-  ], 
-  build: { 
+export default defineConfig({
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects',
+          dest: '.'
+        }
+      ]
+    })
+  ],
+  build: {
     outDir: 'build',
     rollupOptions: {
       input: {
-        main: 'index.html',
-        landing: 'landing-page.html'
+        main: 'index.html'
       }
     }
-  }, 
+  },
 });
